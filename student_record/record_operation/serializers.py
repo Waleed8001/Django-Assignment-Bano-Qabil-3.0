07a4f8b33b38c100c6f694238dc1f1ac494c3a14
Muhammad Waleed Kamal <157250005+Waleed8001@users.noreply.github.com>
2024-09-14 18:29:39 +0500

    Update serializers.py

--- student_record/record_operation/serializers.py
+++ student_record/record_operation/serializers.py
@@ -4,6 +4,7 @@ from rest_framework_mongoengine.serializers import DocumentSerializer
 # DocumentSerializers is used for taking data by using rest_framework_mongoengine because our data 
 
 # store in MongoDB and by using mongoengine we can easily save our data in MongoDB
+# or we can use serializers.DocumentSerializer if we use from rest_framework_mongoengine import serializers.DocumentSerializer
 
 class StudentSer(DocumentSerializer):
     class Meta:
